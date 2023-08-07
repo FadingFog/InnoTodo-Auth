a102786323b24413f4ebb24b14c75e86a624ab15
FadingFog <aleksey.guzlov@gmail.com>
2023-08-07 17:22:32 +0300

    Configured CORS middleware

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import routes
 from app.db import engine
 from app.models.base import Base
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+headers = ["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Authorization"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=headers,
+    expose_headers=["*"],
+)
 
 
 @app.on_event("startup")
